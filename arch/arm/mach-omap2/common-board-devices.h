32c3282ff1506036d8d1bb68f1885dd4ca071fb5
Peter Ujfalusi <peter.ujfalusi@ti.com>
2011-07-24 20:59:33 +0100

    Subject: [PATCH 038/104] OMAP4: Move common twl6030 configuration to twl-common
    Reduce the amount of duplicated code by moving the common
    configuration for TWL6030 (on OMAP4 platform) to the
    twl-common file.
    Use the omap4_pmic_get_config function from board files to
    properly configure the PMIC with the common fields.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- arch/arm/mach-omap2/common-board-devices.h
+++ arch/arm/mach-omap2/common-board-devices.h
@@ -3,6 +3,23 @@
 
 #define NAND_BLOCK_SIZE	SZ_128K
 
+#define TWL_COMMON_PDATA_USB		(1 << 0)
+
+/* Common LDO regulators for TWL4030/TWL6030 */
+#define TWL_COMMON_REGULATOR_VDAC	(1 << 0)
+#define TWL_COMMON_REGULATOR_VAUX1	(1 << 1)
+#define TWL_COMMON_REGULATOR_VAUX2	(1 << 2)
+#define TWL_COMMON_REGULATOR_VAUX3	(1 << 3)
+
+/* TWL6030 LDO regulators */
+#define TWL_COMMON_REGULATOR_VMMC	(1 << 4)
+#define TWL_COMMON_REGULATOR_VPP	(1 << 5)
+#define TWL_COMMON_REGULATOR_VUSIM	(1 << 6)
+#define TWL_COMMON_REGULATOR_VANA	(1 << 7)
+#define TWL_COMMON_REGULATOR_VCXIO	(1 << 8)
+#define TWL_COMMON_REGULATOR_VUSB	(1 << 9)
+#define TWL_COMMON_REGULATOR_CLK32KG	(1 << 10)
+
 struct twl4030_platform_data;
 struct mtd_partition;
 
@@ -34,4 +51,8 @@ void omap_ads7846_init(int bus_num, int gpio_pendown, int gpio_debounce,
 		       struct ads7846_platform_data *board_pdata);
 void omap_nand_flash_init(int opts, struct mtd_partition *parts, int n_parts);
 
+void omap4_pmic_get_config(struct twl4030_platform_data *pmic_data,             
+                           u32 pdata_flags, u32 regulators_flags);  
+
 #endif /* __OMAP_COMMON_BOARD_DEVICES__ */
+
